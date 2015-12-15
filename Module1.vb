571c613df454441b929e720ea20527b1394a2f42
carlosmast23 <carlosmast2301@hotmail.es>
2015-12-15 08:20:27 -0500

    primera versión

---
+++ Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim sc As SocketCliente = New SocketCliente("127.0.0.1", 9999)
+        Console.WriteLine("presione una tecla para iniciar la conexion ...")
+        Console.WriteLine("cliente conectado con el servidor...")
+        sc.conectar()
+        While True
+            Console.WriteLine("Ingrese el mensaje para enviar: ")
+            Dim msj As String = Console.ReadLine()
+            sc.salida(msj)
+
+            If msj = "EXIT" Then
+                Exit While
+            Else
+                Console.WriteLine(sc.leer())
+            End If
+
+        End While
+
+    End Sub
+
+End Module
---
+++ socket/SocketCliente.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+
+Public Class SocketCliente
+    Private clientSocket As Socket
+    Private serverAddress As IPEndPoint
+    Private host As String
+    Private puerto As Integer
+
+    Public Sub New(ByRef host As String, ByRef puerto As Integer)
+        Me.host = host
+        Me.puerto = puerto
+    End Sub
+
+    Public Sub conectar()
+        Try
+            Me.serverAddress = New IPEndPoint(IPAddress.Parse(host), puerto)
+            Me.clientSocket = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+            Me.clientSocket.Connect(Me.serverAddress)
+        Catch ex As SocketException
+            Throw New Exception("El socket no se pudo conectar")
+        End Try
+
+
+    End Sub
+
+    Public Function leer() As String
+        Dim sizeBuffer(1024) As Byte
+        Dim tamanio As Integer = clientSocket.Receive(sizeBuffer)
+        Dim myString As String = Encoding.UTF8.GetString(sizeBuffer, 0, tamanio)
+        Return myString
+    End Function
+
+    Public Function salida(ByRef texto As String)
+        Dim msg() As Byte = Encoding.UTF8.GetBytes(texto)
+        clientSocket.Send(msg)
+        Return True
+    End Function
+
+    Public Sub desconectar()
+        clientSocket.Close()
+    End Sub
+End Class
